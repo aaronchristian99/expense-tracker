91adb4c136d9014ecf7dab55a9e3b9de1af34653
Mele . <mele.felix@ontariotechu.net>
2025-01-28 15:51:44 -0500

    added toCSV method (and added back the entire class i accidentally nuked

--- Expense.cpp
+++ Expense.cpp
@@ -1,20 +1,34 @@
-#ifndef EXPENSE_H
-#define EXPENSE_H
+#pragma once
 
+#include <array>
 #include <string>
-using namespace std;
+#include "currentDate.cpp"
 
 class Expense {
     private:
-        int id, date, month, year;
+        int id, day, month, year;
         string desc;
         double amount;
+
     public:
-        Expense(string desc, double amount, int date, int month, int year) : desc(desc), amount(amount), date(date), month(month), year(year) {};
+        Expense(string desc, double amount, int day = -1, int month = -1, int year = -1) : desc(desc), amount(amount), day(day), month(month), year(year) {
+            
+            if(day == -1 || month == -1 || year == -1){
+                array<int, 3> currentDate = getCurrentDate();
+                this->day = currentDate[0];
+                this->month = currentDate[1];
+                this->year = currentDate[2];
+            }else{
+                this->day = day;
+                this->month = month;
+                this->year = year;
+            }
+        };
 
         string toString() {
-            return "$" + to_string(amount) + desc + to_string(date) + "/" + to_string(month) + "/" + to_string(year);
+            return "$" + to_string(amount) + " " + desc + " " + to_string(day) + "/" + to_string(month) + "/" + to_string(year);
         };
+        string toCSV() {
+            return desc + ',' + to_string(amount) + ',' + to_string(day) + ',' + to_string(month) + ',' + to_string(year);
+        }
 };
-
-#endif // GRANDPARENT_H
