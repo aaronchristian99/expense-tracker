a77fe273f47da6599d04410b7969e28b6e9ab542
Aaron Christian <60152828+aaronchristian99@users.noreply.github.com>
2025-01-28 08:45:56 -0500

    main - Added main.cpp file

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "Expense.cpp"
+using namespace std;
+
+int main() {
+    int option;
+    Expense expense;
+
+    while (option != 5) {
+        std::cout << "------------------ Expense Tracker ---------------------" << std::endl;
+        std::cout << "1. View Expense" << std::endl << "2. Add Expense" << std::endl << "3. Filter Expense" << std::endl << "4. Save to File" << std::endl
+                  << "5. Visualize" << std::endl << "6. Exit" << std::endl;
+        std::cin >> option;
+
+        switch (option) {
+            case 1:
+                expense.viewExpense();
+                break;
+
+            case 2:
+                expense.addExpense();
+                break;
+
+            case 3:
+                expense.filterExpense();
+                break;
+
+            case 4:
+                expense.saveToFile();
+                break;
+
+            case 5:
+                expense.visualize();
+                break;
+
+            default:
+                cout << "Invalid option." << endl;
+                break;
+        }
+    }
+}
