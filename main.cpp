65ea42d1bbabb87d9370b0d16cb2bb1e63b9f20a
otuMoment <rahif.haffeez@ontariotechu.net>
2025-01-28 16:19:01 -0500

    main.cpp got reverted

--- main.cpp
+++ main.cpp
@@ -1,40 +1,83 @@
 #include <iostream>
+#include <vector> 
 #include "Expense.cpp"
 using namespace std;
 
 int main() {
+    //Test code
+    // Expense expense1("test", 45.75);
+    // Expense expense2("Grocery Shopping",500, 24,2,2020);
+    // cout << expense1.toString() << endl;
+    // cout << expense2.toString() << endl;
+
+    vector<Expense> expenseList;
+    int numOfExpenses = 1;
     int option;
+    string userInput;
+    bool run = true;
+    cout << "------------------ Expense Tracker ---------------------" << endl;
 
-    while (option != 5) {
-        std::cout << "------------------ Expense Tracker ---------------------" << std::endl;
-        std::cout << "1. View Expense" << std::endl << "2. Add Expense" << std::endl << "3. Filter Expense" << std::endl << "4. Save to File" << std::endl
-                  << "5. Visualize" << std::endl << "6. Exit" << std::endl;
-        std::cin >> option;
+    while(run == true){
+        int day, month, year;
+        float amount;
+        string desc;
 
-        switch (option) {
-            case 1:
-                // expense.viewExpense();
+        cout << "1. Add Expense" << endl << "2. View Expenses" << endl << "3. Save to File" << endl << "4. Exit Program" << endl << "Enter your input: ";
+        cin >> option;
+
+        switch(option){ //i just threw everything in here to just set things up
+            case 1: //Add Expense
+
+                cout << "Please enter the description of the expense: " << endl;
+                cin.ignore();
+                getline(cin, desc);
+                cout << "Please enter the amount of the expense: " << endl;
+                cin >> amount;
+
+                while(true){
+                    cout << "Would you like to use todays date? (y/n)." << endl;
+                    cin >> userInput; 
+
+                    if(userInput == "y"){
+                        expenseList.push_back(Expense(desc,amount));
+                        cout << "Expense added." << endl;;
+                        numOfExpenses++;
+                        break;
+                    
+                    }else if(userInput == "n"){ //this is awful ik, could make some date parser later on
+                        cout << "Please enter the day (dd)"  << endl;
+                        cin >> day;
+                        cout << "Please enter the month (mm)" << endl;
+                        cin >> month;
+                        cout << "Please enter the year (yyyy)" << endl;
+                        cin >> year;
+                        expenseList.push_back(Expense(desc,amount,day,month,year));
+                        cout << "Expense added." << endl;
+                        numOfExpenses++;
+                        break;
+                    
+                    }else{
+                        cout << "Invalid option" << endl;
+                    }
+                }
                 break;
 
-            case 2:
-                // expense.addExpense();
+            case 2: //View Expenses
+                cout << "List of expenses: " << endl;
+                for(int i = 0; i < numOfExpenses; i++){
+                    cout << "ID: " << i << ", " << expenseList[i].toString() << endl;
+                }
                 break;
-
-            case 3:
-                // expense.filterExpense();
+            
+            case 3: //Save to File
                 break;
-
-            case 4:
-                // expense.saveToFile();
+            
+            case 4: //Exit Program
+                cout << "See you later neeeeeeeeerd";
+                run = false;
                 break;
-
-            case 5:
-                // expense.visualize();
-                break;
-
-            default:
-                cout << "Invalid option." << endl;
-                break;
-        }
+        }        
     }
+    
+    return 0;
 }
