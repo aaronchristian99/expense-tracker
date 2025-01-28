e7126ba7737e38dc20b1292986d6e9329b518e2e
Aaron Christian <60152828+aaronchristian99@users.noreply.github.com>
2025-01-28 09:18:55 -0500

    main - Added Expense.h file

---
+++ Expense.h
@@ -0,0 +1,19 @@
+#ifndef EXPENSE_H
+#define EXPENSE_H
+
+#include <string>
+
+class Expense {
+    private:
+        int id, date, month, year;
+        string desc;
+        double amount;
+    public:
+        Expense(string desc, double amount, int date, int month, int year) : desc(desc), amount(amount), date(date), month(month), year(year) {};
+
+        string toString() {
+            return "$" + to_string(amount) + desc + to_string(date) + "/" + to_string(month) + "/" + to_string(year);
+        };
+}
+
+#endif //EXPENSE_H
