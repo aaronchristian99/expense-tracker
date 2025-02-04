753480bd5058873d2c731a1edab2150dc2d235c1
Mele . <mele.felix@ontariotechu.net>
2025-02-04 08:47:35 -0500

    added unix timestamp method to currentDate

--- currentDate.cpp
+++ currentDate.cpp
@@ -8,3 +8,14 @@ std::array<int, 3> getCurrentDate() {
 
     return {now->tm_mday, now->tm_mon + 1, now->tm_year + 1900};
 }
+
+// returns unix timestamp of date
+long getUnixTime(int day, int month, int year) {
+    struct tm timeStruct = {};
+    
+    timeStruct.tm_mday = day;
+    timeStruct.tm_mon = month - 1;
+    timeStruct.tm_year = year - 1900;
+
+    return mktime(&timeStruct);
+}
